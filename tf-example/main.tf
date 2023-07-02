cef87f87005ab67d62afea06ce1a37aa83904481
Priya Singh <priyajsr65@gmail.com>
2023-07-02 13:37:29 +0530

    Update main.tf

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "exm " {
+resource "aws_instance" "first_instance" {
   ami           = "ami-024e6efaf93d85776"                             # "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
 
